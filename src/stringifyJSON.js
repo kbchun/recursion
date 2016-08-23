7d15a9a0bd4bcb20600d1f30ae4378531a3d1802
Kyle Chun <kbchun5712@gmail.com>
2016-08-22 21:06:38 -0700

    Implemented stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,37 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === "string") {
+  	return '"' + obj + '"';
+  }
+  else if (Array.isArray(obj)) {
+    var result = "";
+    for (var i = 0; i < obj.length; i++) {
+      result += stringifyJSON(obj[i]);
+      if (i !== obj.length - 1) {
+        result += ",";
+      }
+    }
+    return "[" + result + "]";
+  }
+  else if (typeof obj === "object" && obj) {
+  	var result = "";
+    var count = 0;
+    var val;
+    for (var key in obj) {
+  	  if (obj[key] === undefined || typeof obj[key] === "function") {
+	  	return "{}";
+	  }
+      val = stringifyJSON(obj[key]);
+  	  result += stringifyJSON(key) + ":" + val;
+  	  count++;
+  	  if (count !== Object.keys(obj).length) {
+  		result += ",";
+  	  }
+    }
+    return "{" + result + "}";
+  }
+  else {
+    return "" + obj;
+  }
 };
